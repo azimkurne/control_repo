87469c39221e5cfb34be3056d8564b2b4ac5038e
Azim Kurne <34882603+azimkurne@users.noreply.github.com>
2019-04-11 12:37:43 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,33 @@
 node default {
 }
+
+
 node 'master.puppet.vm' {
+ 
+ file { '/tmp/resource_file_test.txt':
+    ensure  => file,
+    content => 'This is a resource file creation testing',
+    owner   => 'root',
+  }
+  
   include role::master_server
   file { '/root/README':
      ensure  => file,
      content => "Welcome to ${fqdn}\n",
   }
 }
+
+
 node 'minetest.puppet.vm'{
  include role::minecraft_server
 }
+
+
 node /^web/ {
  include role::app_server
 }
+
+
 node /^db/ {
  include role::db_server
-
 }
