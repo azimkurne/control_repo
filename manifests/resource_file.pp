7cc06174bcc02f5d5d6a5dd067b61789b6a61663
Azim Kurne <34882603+azimkurne@users.noreply.github.com>
2019-04-11 12:31:38 +0530

    Update resource_file.pp

--- manifests/resource_file.pp
+++ manifests/resource_file.pp
@@ -1,5 +1,7 @@
 node default {
-  ensure  => file,
-  content => 'This is a resource file for testing',
-  owner   => 'root',
+  file { '/tmp/resource_file_test.txt':
+    ensure  => file,
+    content => 'This is a resource file for testing',
+    owner   => 'root',
+  }
 }
